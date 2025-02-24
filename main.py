7a1398407b8038d4e4ac68316a2fc22472673176
Bugrova Irina <bugrova147@yandex.ru>
2025-02-24 21:29:53 +0300

    add main.py with func for user_interaction

---
+++ main.py
@@ -0,0 +1,40 @@
+from src.api_connection import HeadHunter
+from src.utils import FileRepository
+from src.vacancy import Vacancy
+
+
+def user_interaction():
+    """
+    Запрос данных у пользователя, вывод результатов работы программы
+    """
+    user_keyword = (input("Введите слово для поиска вакансии\n")).lower()
+    user_top_n = int(input("Введите количество вакансий, которые нужно вывести\n"))
+
+    # Получение данных с hh.ru
+    vacancies_from_hh = HeadHunter(
+        "https://api.hh.ru/vacancies",
+        {'User-Agent': 'HH-User-Agent'},
+        {
+            'text': user_keyword,
+            'page': 0,
+            'per_page': 100,
+        },
+        'Moscow',
+        1,
+        10,
+        user_keyword
+    )
+    formated_vacancies = vacancies_from_hh.get_vacancies()
+    # print(formated_vacancies)
+
+    # Сохраняем вакансии в файл
+    file_repository = FileRepository('../coursework_oop/data/data.json')
+    file_repository.add(formated_vacancies)
+
+    # Фильтруем вакансии
+    filtered_vacancies = file_repository.get_by_filters(top_n=user_top_n, keyword=user_keyword)
+    print(filtered_vacancies)
+
+
+if __name__ == '__main__':
+    user_interaction()
